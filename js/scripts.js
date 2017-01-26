9821c891bac1c2457054329e75d936dcb7877998
melanietyler <melanie.leann.tyler@gmail.com>
2017-01-25 19:52:22 -0500

    lets try again

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
+// when we scroll X pixels, slide down the menu
 
+//$('.bar').slideDown(2000);
+
+//when I click the logo, open the menu
+
+$('.logo').click (function () {
+  $('.bar').addClass('exposed');
+
+});
+
+$(window).scroll(function () {
+
+// determube hoq mmany pizes have been scrolled from teh top
+var px = $(window).scrollTop()
+
+console.log("Pixels scrolled: " + px);
+
+// if the number of pixels scrolled is a full screen height, expose the manu bar
+ if (px >= 953) {
+   //open the menu bar
+   $('.bar').addClass('exposed');
+ }
+
+ else {
+   $('.bar').removeClass('exposed');
+ }
+
+});
